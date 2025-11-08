2d0a0ca65169bbf3b716ab202bbd81c1a77017e8
sarli-s <sarli37531@gmail.com>
2025-11-09 00:50:32 +0200

    first upload

---
+++ WebAPIShop/Controllers/UsersController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPIShop.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        //string filePath= "M:\\WebApi\\daf.txt";
+        string filePath = "C:\\Users\\תמר שפריי\\Documents\\שרלי לימודים\\סופי\\users.txt";// "C:\\Users\\user\\Desktop\\daf.txt";
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+      
+        [HttpGet("{id}")]
+        public ActionResult<User> Get(int id) {
+            using (StreamReader reader = System.IO.File.OpenText(filePath))
+            {
+                string? currentUserInFile;
+                while ((currentUserInFile = reader.ReadLine()) != null)
+                {
+                    User user = JsonSerializer.Deserialize<User>(currentUserInFile);
+                    if (user.UserId == id)
+                        return user;
+                }
+            }
+            return NoContent();   /////
+        }
+  
+        [HttpPost]
+        public ActionResult<User> Post([FromBody] User user)
+        {
+            int numberOfUsers = System.IO.File.ReadLines(filePath).Count();
+            user.UserId = numberOfUsers + 1;
+            string userJson = JsonSerializer.Serialize(user);
+            System.IO.File.AppendAllText(filePath, userJson + Environment.NewLine);
+            return CreatedAtAction(nameof(Get), new
+            {
+                id = user.UserId
+            }, user);
+        }
+
+
+        [HttpPost("login")]
+        public ActionResult<User> Post([FromBody] LoginUser UserR)
+        {
+
+            using (StreamReader reader = System.IO.File.OpenText(filePath))
+            {
+                string? currentUserInFile;
+                while ((currentUserInFile = reader.ReadLine()) != null)
+                {
+                    User userT = JsonSerializer.Deserialize<User>(currentUserInFile);
+                    if (userT.UserEmail == UserR.LoginUserEmail && userT.UserPassword == UserR.LoginUserPassword)
+                        return CreatedAtAction(nameof(Get), new { id = userT.UserId }, userT);
+                }
+            }
+            return NoContent();   /////
+        }
+       
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] User value)
+        {
+            string textToReplace = string.Empty;
+            using (StreamReader reader = System.IO.File.OpenText(filePath))
+            {
+                string currentUserInFile;
+                while ((currentUserInFile = reader.ReadLine()) != null)
+                {
+
+                    User user = JsonSerializer.Deserialize<User>(currentUserInFile);
+                    if (user.UserId == id)
+                        textToReplace = currentUserInFile;
+                }
+            }
+
+            if (textToReplace != string.Empty)
+            {
+                string text = System.IO.File.ReadAllText(filePath);
+                text = text.Replace(textToReplace, JsonSerializer.Serialize(value));
+                System.IO.File.WriteAllText(filePath, text);
+            }
+
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebAPIShop/wwwroot/home.js
@@ -0,0 +1,82 @@
+﻿const userEmail = document.querySelector("#userName")
+const firstName = document.querySelector("#firstName")
+const lastName = document.querySelector("#lastName")
+const password = document.querySelector("#password")
+function validateEmail(email) {
+    return email.includes("@") && email.includes(".");
+}
+
+const register = async () => {
+    try {
+        if (!validateEmail(userEmail.value)) {
+            alert("כתובת האימייל אינה תקינה");
+            return;
+        }
+
+        if (password.value.length < 4 || password.value.length > 8) {
+            alert("הסיסמה חייבת להיות בין 4 ל-8 תווים");
+            return;
+        }
+        const newUser = {
+            UserId: 0,
+            UserEmail: userEmail.value,
+            UserFirstName: firstName.value,
+            UserLastName: lastName.value,
+            UserPassword: password.value
+        }
+        const response = await fetch('https://localhost:44324/api/Users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newUser)
+        });
+        if (response.ok) {
+            const dataPost = await response.json();
+
+            alert("💕 נרשמת בהצלחה")
+        }
+        else {
+            alert(" 😒 מצטערים, הרשמתך לא נקלטה")
+        }
+    } catch (err) {
+        alert(err)
+    }
+}
+
+
+
+const loginUserEmail = document.querySelector("#userNameR")
+const loginUserPassword = document.querySelector("#passwordR")
+
+const login = async () => {
+    try {
+        const loginUser = {
+            LoginUserEmail: loginUserEmail.value,
+            LoginUserPassword: loginUserPassword.value
+        }
+        const response = await fetch('https://localhost:44324/api/Users/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(loginUser)
+        });
+        if (response.status == 204) {   /////
+            alert(" 😒 אתה עדייין לא קיים במערכת נא הרשם")
+        }
+
+        else {
+            alert("💕  התחברת בהצלחה")
+            const currentUser = await response.json();
+            sessionStorage.setItem("currentUser", JSON.stringify(currentUser))
+            window.location.href = "update.html"
+        }
+    } catch (err) {
+        alert(err)
+    }
+
+
+}
+
+
