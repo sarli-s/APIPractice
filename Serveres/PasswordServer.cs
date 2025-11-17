94dcf2fc97f421c95134832ca5e1b7bdf8b39093
sarli-s <sarli37531@gmail.com>
2025-11-18 01:53:10 +0200

    Adding a password strength checking mechanism

---
+++ Serveres/PasswordServer.cs
@@ -0,0 +1,22 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zxcvbn;
+
+namespace Serveres
+{
+    public class PasswordServer
+    {
+        public Password checkPassword(string passwordGet)
+        {
+            Password password = new Password();
+            password.PasswordItself=passwordGet;
+            var result = Zxcvbn.Core.EvaluatePassword(password.PasswordItself);
+            password.Level = result.Score;
+            return password;
+        }
+}
+}
---
+++ WebAPIShop/Controllers/PasswordController.cs
@@ -0,0 +1,46 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Serveres;
+
+namespace WebAPIShop.Controllers
+{
+    public class PasswordController : Controller
+    {
+
+        PasswordServer server = new PasswordServer();
+
+        //[Route("api/[controller]")]
+
+        //[HttpGet]
+        //public ActionResult<Password> Get()
+        //{
+        //    Password password = new Password() { PasswordItself = "123" };
+        //    if (password != null)
+        //    {
+        //        return Ok(password);
+        //    }
+        //    return NoContent();
+        //}
+        [Route("api/[controller]")]
+
+        [HttpPost]
+        public ActionResult<Password> Post([FromBody] Password password)
+        {
+            password= server.checkPassword(password.PasswordItself);
+            return Ok(password);
+            //Password creatingPassword = password;
+
+            //return CreatedAtAction(nameof(Get), new
+            //{
+            //    id = creatingPassword.PasswordItself
+            //}, creatingPassword);
+        }
+       
+
+
+
+
+    }
+}
